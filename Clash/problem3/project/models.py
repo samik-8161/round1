69d7965009f78ee494a86e134357477b7f5389b7
tanmaypardeshi <tanmaypardeshi@gmail.com>
2019-08-21 00:21:04 +0530

    Login_Logout

--- Clash/problem3/project/models.py
+++ Clash/problem3/project/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Profile(models.Model):
     p1_name = models.CharField(max_length=100)
-    p2_name = models.CharField(max_length=100)
     p1_email = models.CharField(max_length=100)
-    p2_email = models.CharField(max_length=100)
     mob1 = models.CharField(max_length=12)
-    mob2 = models.CharField(max_length=12)
+    p2_name = models.CharField(max_length=100, default="a")
+    p2_email = models.CharField(max_length=100, default="a@a.com")
+    mob2 = models.CharField(max_length=12,default="9999999999")
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
@@ -21,6 +22,10 @@ class Questions(models.Model):
 
     def __str__(self):
         return self.question
+
+class login_logout(models.Model):
+    login_time=models.DateTimeField(max_length=100,default="")
+    logout_time=models.DateTimeField(max_length=100,default="")
 
 class Score(models.Model):
     count = models.IntegerField(default=0)
--- Clash/problem3/project/views.py
+++ Clash/problem3/project/views.py
@@ -2,12 +2,11 @@ import datetime
 import random
 import re
 
-from django.contrib import auth, messages
+from django.contrib import auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth import login, logout, authenticate
-import datetime
+from django.contrib.auth import login, logout
 
 from .models import Questions, Profile, Score
 
@@ -27,13 +26,13 @@ def index1(request):
             except User.DoesNotExist:
                 user = User.objects.create_user(username=request.POST['uname'], password=request.POST['pass'])
                 p1_name = request.POST['p1_name']
-                p2_name = request.POST['p2_name']
                 p1_email = request.POST['p1_email']
-                p2_email = request.POST['p2_email']
                 mob1 = request.POST['mob1']
+                p2_name = request.POST['p2_name']
+                p2_email = request.POST['p2_email']
                 mob2 = request.POST['mob2']
 
-                userprofile = Profile(p1_name=p1_name, p2_name=p2_name, p1_email=p1_email, p2_email=p2_email, mob1=mob1,
+                userprofile = Profile(p1_name=p1_name, p1_email=p1_email, mob1=mob1, p2_name=p2_name, p2_email=p2_email,
                                       mob2=mob2, user=user)
 
             if re.search(regex, p1_email and p2_email):
@@ -92,10 +91,9 @@ def index3(request, qno):
     return redirect(reverse('index2'))
 
 
-def login_logout(request):
-
-    login = datetime.datetime.now().time(request.POST.get('login'))
-    logout = datetime.datetime.now().time(request.POST.get('logout'))
+def login_logout(request,login_time,logout_time):
+    login_time = datetime.datetime.now()
+    logout_time = datetime.datetime.now()
     context = {'login': login, 'logout': logout, 'score': counter}
     return render(request, 'Result_page.html', context)
 
